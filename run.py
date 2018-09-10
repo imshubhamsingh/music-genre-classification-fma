54abf1daf01b10b7f2d75bba2c72f066238344fa
Shubham Singh <imshubhamsingh97@gmail.com>
2018-09-11 00:47:18 +0530

    fixed bugs

--- run.py
+++ run.py
@@ -1,8 +1,20 @@
 import numpy as np
+import pandas as pd
 from libs import utils, dataset, process, model
+
+metadata_genre_file_path = './dataset/fma_metadata/tracks.csv'
+subset = 'small'
+utils.get_music_genre(metadata_genre_file_path, subset)
+labels = pd.read_csv('preprocessing/train_labels.csv')
+
 
 genres = np.array(['Hip-Hop', 'Pop', 'Folk', 'Experimental', 'Rock', 'International',
                    'Electronic', 'Instrumental'])
+
+
+dataset_path = 'dataset/fma_small'
+utils.get_all_music(dataset_path, labels, 20, 200)
+utils.slice_spectrograms("spectrograms", 198)
 
 train_x, train_y, validation_x, \
     validation_y, test_x, test_y = dataset.create_dataset_from_slices(
